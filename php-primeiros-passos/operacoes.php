e101c66ea2a6a0c4a11bd1a90c71283aac20fb3c
Daniel Silveira <danielsilveira.contato@gmail.com>
2021-05-20 14:48:44 -0300

    Introdução ao PHP - Variáveis e tipos

---
+++ php-primeiros-passos/operacoes.php
@@ -0,0 +1,13 @@
+<?php
+// $idade = 36;
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+$doisAoCubo = 2 ** 2;
+$restoDaDivisao = 10 % 3;
+
+// $idadedaqui10Anos = $idade + 10;
+// $idadedaqui10Anos = $idade + 10;
+
+// echo $idadedaqui10Anos;
